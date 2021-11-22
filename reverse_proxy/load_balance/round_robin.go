b4926d7164aa78edaeea7728fbc4549dd4a00134
sky-bai <569531262@qq.com>
2021-11-22 23:06:11 +0800

    加权轮询

---
+++ reverse_proxy/load_balance/round_robin.go
@@ -0,0 +1,45 @@
+package load_balance
+
+import (
+	"errors"
+)
+
+// RoundRobinBalance 轮询负载均衡
+type RoundRobinBalance struct {
+	curIndex int
+	rss      []string
+
+	// 这里我需要维护下游服务器列表
+	conf LoadBalanceZkConfInterface
+}
+
+func (r *RoundRobinBalance) Get(s string) (string, error) {
+	panic("implement me")
+}
+
+func (r *RoundRobinBalance) Update() {
+	panic("implement me")
+}
+
+func (r *RoundRobinBalance) Add(params ...string) error {
+	if len(params) == 0 {
+		return errors.New("params is empty")
+	}
+	r.rss = append(r.rss, params...)
+	return nil
+}
+
+func (r *RoundRobinBalance) Next() string {
+	if len(r.rss) == 0 {
+		return ""
+	}
+	if r.curIndex >= len(r.rss) {
+		r.curIndex = 0
+	}
+	r.curIndex++
+	return r.rss[r.curIndex-1]
+}
+
+func (r *RoundRobinBalance) SetConf(conf LoadBalanceZkConfInterface) {
+	r.conf = conf
+}
