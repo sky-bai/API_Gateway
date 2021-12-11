a03e470de404b2608c70a1ac8655c48b655ca50e
sky-bai <569531262@qq.com>
2021-12-11 15:36:11 +0800

    删除租户信息

--- api/internal/logic/app/deleteapplogic.go
+++ api/internal/logic/app/deleteapplogic.go
@@ -1,12 +1,14 @@
 package app
 
 import (
+	"API_Gateway/api/internal/middleware"
 	"context"
+	"errors"
 
 	"API_Gateway/api/internal/svc"
 	"API_Gateway/api/internal/types"
-
 	"github.com/tal-tech/go-zero/core/logx"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type DeleteAppLogic struct {
@@ -23,8 +25,30 @@ func NewDeleteAppLogic(ctx context.Context, svcCtx *svc.ServiceContext) DeleteAp
 	}
 }
 
+// DeleteApp 删除租户
 func (l *DeleteAppLogic) DeleteApp(req types.UpdateAppRequest) (*types.AppResponse, error) {
-	// todo: add your logic here and delete this line
+	// 1.校验参数
+	errMessage := ErrorString{errMessage: ""}
+	err := middleware.ValidatorHandler.Validate.Struct(&req)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		for _, errValue := range errs.Translate(middleware.ValidatorHandler.Translate) {
+			errMessage.errMessage += " " + errValue
+		}
+		return nil, &errMessage
+	}
 
-	return &types.AppResponse{}, nil
+	// 2.查看是否有该租户
+	_, err = l.svcCtx.GatewayAppModel.FindOne(req.ID)
+	if err != nil {
+		return nil, errors.New("查询该租户信息失败")
+	}
+
+	// 3.删除租户
+	err = l.svcCtx.GatewayAppModel.Delete(req.ID)
+	if err != nil {
+		return nil, errors.New("删除该租户失败")
+	}
+
+	return &types.AppResponse{Message: "删除该租户信息成功"}, nil
 }
