6c848acec1cf4845049dc2450e660c1c83b88905
sky-bai <569531262@qq.com>
2021-12-11 15:30:54 +0800

    添加租户列表

---
+++ api/internal/handler/app/applisthandler.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"net/http"
+
+	"API_Gateway/api/internal/logic/app"
+	"API_Gateway/api/internal/svc"
+	"API_Gateway/api/internal/types"
+	"github.com/tal-tech/go-zero/rest/httpx"
+)
+
+func AppListHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.AppListRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := app.NewAppListLogic(r.Context(), ctx)
+		resp, err := l.AppList(req)
+		reponse.Response(w, resp, err) //②
+	}
+}
