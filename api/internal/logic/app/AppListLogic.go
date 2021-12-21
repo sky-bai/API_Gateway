7d2738d0569c1131814fc933c50fa40176293f57
sky-bai <569531262@qq.com>
2021-12-21 19:42:18 +0800

    租户列表

---
+++ api/internal/logic/app/AppListLogic.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"API_Gateway/model/ga_gateway_app"
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/tal-tech/go-zero/core/stores/sqlc"
+
+	"API_Gateway/api/internal/svc"
+	"API_Gateway/api/internal/types"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type AppListLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewAppListLogic(ctx context.Context, svcCtx *svc.ServiceContext) AppListLogic {
+	return AppListLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *AppListLogic) AppList(req types.AppListRequest) (*types.APPListResponse, error) {
+	appInfo, err := l.svcCtx.GatewayAppModel.GetServiceList(req.Info, req.PageNo, req.PageSize)
+	if err != nil && err != sqlc.ErrNotFound {
+		fmt.Println(err)
+		return nil, errors.New("获取租户列表失败")
+	}
+
+	appList := appInfo.Data.([]ga_gateway_app.GatewayApp)
+
+	var list []types.APPListItemOutput
+	for _, app := range appList {
+		list = append(list, types.APPListItemOutput{
+			ID:       app.Id,
+			AppID:    app.AppId,
+			Name:     app.Name,
+			Secret:   app.Secret,
+			WhiteIPS: app.WhiteIps,
+			Qpd:      app.Qpd,
+			Qps:      app.Qps,
+			RealQpd:  0,
+			RealQps:  0,
+		})
+	}
+
+	return &types.APPListResponse{List: list, Total: 0}, nil
+}
