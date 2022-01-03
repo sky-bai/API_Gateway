f8d1962d05a28b68622dceb5bd016917f4c0e3d0
sky-bai <569531262@qq.com>
2022-01-03 19:46:35 +0800

    URLwrite重写

--- api/internal/middleware/UrlrewriteMiddleware.go
+++ api/internal/middleware/UrlrewriteMiddleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"API_Gateway/api/internal/global"
+	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
@@ -27,13 +28,15 @@ func (m *UrlRewriteMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 			}
 			regexp, err := regexp.Compile(items[0])
 			if err != nil {
-				//fmt.Println("regexp.Compile err",err)
+				fmt.Println("regexp.Compile err", err)
 				continue
 			}
-			//fmt.Println("before rewrite",c.Request.URL.Path)
+			fmt.Println("Url 重写之前的 path", r.URL.Path)
+			fmt.Println("重写的规则", regexp)
+			fmt.Println("重写规则", items[1])
 			replacePath := regexp.ReplaceAll([]byte(r.URL.Path), []byte(items[1]))
 			r.URL.Path = string(replacePath)
-			//fmt.Println("after rewrite",c.Request.URL.Path)
+			fmt.Println("Url 重写之后的 path", r.URL.Path)
 		}
 		next(w, r)
 	}
