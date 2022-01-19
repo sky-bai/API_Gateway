1a46a56f8c7cce98d90e21cda9a7a636d795a4d5
sky-bai <569531262@qq.com>
2022-01-19 09:54:33 +0800

    加权轮询

--- api/internal/reverse_proxy/load_balance/weight_round_robin.go
+++ api/internal/reverse_proxy/load_balance/weight_round_robin.go
@@ -23,7 +23,7 @@ type WeightRoundRobinBalance struct {
 }
 
 func (w *WeightRoundRobinBalance) Get(s string) (string, error) {
-	fmt.Println("rss:", w.rss)
+	//fmt.Println("rss:", w.rss)
 	return w.Next(), nil
 }
 
@@ -36,13 +36,13 @@ func (w *WeightRoundRobinBalance) Update() {
 		//fmt.Println("WeightRoundRobinBalance get check conf:", conf.GetConf())
 		w.rss = nil
 		for _, ip := range conf.GetConf() {
-			fmt.Println("配置负载均衡器的服务器列表WeightRoundRobinBalance get ip:", ip)
+			//fmt.Println("配置负载均衡器的服务器列表WeightRoundRobinBalance get ip:", ip)
 			err := w.Add(strings.Split(ip, ",")...)
 			if err != nil {
 				fmt.Println("WeightRoundRobinBalance get conf err:", err)
 			}
 		}
-		fmt.Println("该负载均衡器维护的下游服务器列表", w.rss)
+		//fmt.Println("该负载均衡器维护的下游服务器列表", w.rss)
 	}
 }
 
@@ -50,13 +50,13 @@ func (w *WeightRoundRobinBalance) Update() {
 // 最初的时候 每个节点的effectiveWeight 和 currentWeight 分别是 weight
 
 func (w *WeightRoundRobinBalance) Add(params ...string) error {
-	fmt.Println("开始添加服务器")
+	//fmt.Println("开始添加服务器")
 	if len(params) != 2 {
 		return errors.New("param len need 2 ")
 	}
 
-	fmt.Println("params:", params)
-	fmt.Println("params:", params[1])
+	//fmt.Println("params:", params)
+	//fmt.Println("params:", params[1])
 
 	parInt, err := strconv.ParseInt(params[1], 10, 64)
 	if err != nil {
@@ -71,7 +71,7 @@ func (w *WeightRoundRobinBalance) Add(params ...string) error {
 		effectiveWeight: int(parInt),
 	})
 
-	fmt.Println("已配置好的 WeightRoundRobinBalance get rss:", w.rss)
+	//fmt.Println("已配置好的 WeightRoundRobinBalance get rss:", w.rss)
 	return nil
 }
 
@@ -82,8 +82,8 @@ func (w *WeightRoundRobinBalance) Next() string {
 
 	for i := 0; i < len(w.rss); i++ {
 		node := w.rss[i]
-		fmt.Println("node:", node)
-		fmt.Println("node:", node.addr)
+		//fmt.Println("node:", node)
+		//fmt.Println("node:", node.addr)
 		// step1: 统计所有有效权重之和
 		totalWeight += node.Weight
 
