d1661b3bc1d0be1436529bd479973a32b3be4233
sky-bai <569531262@qq.com>
2022-01-20 09:29:10 +0800

    定义观察者操作和负载均衡配置操作

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -32,4 +32,14 @@ type Config struct {
 		Host string
 		Port int
 	}
+
+	//LogConf struct {
+	//	ServiceName         string `json:",optional"`
+	//	Mode                string `json:",default=console,options=console|file|volume"`
+	//	Path                string `json:",default=logs"`
+	//	Level               string `json:",default=info,options=info|error|severe"`
+	//	Compress            bool   `json:",optional"`
+	//	KeepDays            int    `json:",optional"`
+	//	StackCooldownMillis int    `json:",default=100"`
+	//}
 }
--- api/internal/reverse_proxy/load_balance/config.go
+++ api/internal/reverse_proxy/load_balance/config.go
@@ -5,6 +5,25 @@ import (
 	"github.com/e421083458/gateway_demo/proxy/zookeeper"
 )
 
+// Observer 客户端需要一个监听者  监听服务列表变化然后更新服务列表
+type Observer interface {
+	Update()
+	// 提供一个有负载均衡配置的观察者更新接口  下面是使用实例
+}
+
+// LoadBalanceZkConfInterface 配置主题   负载均衡配置绑定一个观察者 这里是服务端注册发现 需要有客户端的观察者
+type LoadBalanceZkConfInterface interface {
+	Attach(o Observer)
+	GetConf() []string
+	WatchConf()
+	UpdateConf(conf []string)
+}
+
+// 也就是说我将自身改变服务器列表的时候 我能够自身update服务器的方法 不管你是什么
+// LoadBalanceConf 是可以监听服务器列表的实体和更新配置的实体
+
+// 也就是说我将两个实体抽象出来 确定了他们的方法
+
 // 现在要实例化负载均衡zk配置 这里面要管理服务器列表 监听服务器列表的变化 通过配置zk服务器信息动态获取到服务器列表 可以获取多个观察者
 // 将观察者 负载均衡方法和负载均衡配置绑定
 // 这里LoadBalanceZkConf 实现了上面LoadBalanceConf的接口
