24a855ba9e0d26cc941d0e8ac6d9c64e7c3fc58c
sky-bai <569531262@qq.com>
2022-01-24 10:25:54 +0800

    随机 更新观察者

--- api/internal/reverse_proxy/load_balance/random.go
+++ api/internal/reverse_proxy/load_balance/random.go
@@ -2,7 +2,6 @@ package load_balance
 
 import (
 	"errors"
-	"fmt"
 	"math/rand"
 	"strings"
 )
@@ -24,13 +23,13 @@ func (r *RandomBalance) Get(s string) (string, error) {
 
 func (r *RandomBalance) Update() {
 	if conf, ok := r.conf.(LoadBalanceConfInterface); ok {
-		fmt.Println("Update get check conf:", conf.GetConf())
+		//fmt.Println("Update get check conf:", conf.GetConf())
 		r.rss = nil
 		for _, ip := range conf.GetConf() {
-			fmt.Println("添加进去的ip:", ip)
+			//fmt.Println("添加进去的ip:", ip)
 			r.Add(strings.Split(ip, ",")[0])
 		}
-		fmt.Println("该负载均衡器维护的下游服务器列表", r.rss)
+		//fmt.Println("该负载均衡器维护的下游服务器列表", r.rss)
 	}
 }
 
@@ -55,7 +54,7 @@ func (r *RandomBalance) Next() string {
 		return ""
 	}
 	r.curIndex = rand.Intn(len(r.rss)) //nolint:gosec
-	fmt.Println("随机获取的下游服务器IP:", r.rss[r.curIndex])
+	//fmt.Println("随机获取的下游服务器IP:", r.rss[r.curIndex])
 	return r.rss[r.curIndex]
 }
 
